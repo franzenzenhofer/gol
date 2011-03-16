55fd1dcec671c1214d0085c853b9f64819ac76ae
Franz Enzenhofer <f.enzenhofer@gmail.com>
2011-03-16 22:23:03 +0100

    cleanup

--- gol.js
+++ gol.js
@@ -3,291 +3,190 @@
 Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overcrowding.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.*/
-var util = require('util');
-var jQuery = require('jquery');  var $ = jQuery;
-
-//controlling if a server broadcast is active
+var util = require("util");
+var jQuery = require("jquery");
+var $ = jQuery;
 var active = false;
-
-var falsy = 0;
-var truly = 1;
-
-var wait = 150; //irelevant // only gol server counts currently
+var wait = 150;
 var x = 100;
 var y = 60;
-//var x = 100;
-//var y = 60;
-//var x = 10;
-//var y = 10;
 var worldsize = x * y;
-var defaultAliveColor = '00FF00';
-
-//generation counter
+var defaultParty = "a";
 var generation = 0;
-var colorO = {};
+var partyO = {};
 
-
-var createCell = function(_alive, _color){
-  var color=undefined;
-  
-  if(!_alive){var alive=falsy;}else{var alive=truly;}
-  if(!_color && alive ){var color=defaultAliveColor};
-  return {
-    'alive': alive,
-    'color': color,
-    'ln':[]
+var createCell = function(_alive, _party) {
+  var party = undefined;
+  if(!_alive) {
+    var alive = false
+  }else {
+    var alive = true
   }
-}
+  if(!_party && alive) {
+    var party = defaultParty
+  }
+  return{alive:alive, party:party, ln:[]}
+};
 
-
-var getAWorld = function(x,y,seed)
-{
+var getAWorld = function(x, y, seed) {
   var w = [];
-  var wsize = x*y;
-  
-  for(var i = 0; i<wsize; i++)
-  {
-    w[i]=createCell(falsy);
+  var wsize = x * y;
+  for(var i = 0;i < wsize;i++) {
+    w[i] = createCell(false)
   }
-  
-  ////console.log('seed:'+seed);
-  
-  if(seed && typeof seed == "number")
-  {
-      seed=Math.floor(seed);
-      ////console.log('floor seed:'+seed);
-      for(var i = 0; i<seed; i++)
-      {
-        r = Math.floor(Math.random()*wsize)
-        if(w[r].alive===falsy)
-        {
-          w[r].alive=truly;
-          w[r].color=defaultAliveColor;
-        }
-        else { i--; }
+  if(seed && typeof seed == "number") {
+    seed = Math.floor(seed);
+    for(var i = 0;i < seed;i++) {
+      r = Math.floor(Math.random() * wsize);
+      if(w[r].alive === false) {
+        w[r].alive = true;
+        w[r].party = defaultParty
+      }else {
+        i--
       }
-  }
-  else if(seed && typeof seed == "object")
-  {
-    for (var color in seed)
-    {
-      //console.log('seed name color '+color);
-      seednr = Math.floor(seed[color]);
-      for(var i = 0; i<seednr; i++)
-      {
-        r = Math.floor(Math.random()*wsize)
-        if(w[r].alive===falsy)
-        {
-          w[r].alive=truly;
-          w[r].color=color;
+    }
+  }else {
+    if(seed && typeof seed == "object") {
+      for(var party in seed) {
+        seednr = Math.floor(seed[party]);
+        for(var i = 0;i < seednr;i++) {
+          r = Math.floor(Math.random() * wsize);
+          if(w[r].alive === false) {
+            w[r].alive = true;
+            w[r].party = party
+          }else {
+            //i--
+          }
         }
-        else { i--; }
       }
     }
   }
-  return w;
-}
-
-//var world = getAWorld(x,y,worldsize/5);
-var world = getAWorld(x,y,{
-'FF0000':worldsize/5,
-'0000FF':worldsize/5,
-'00FF00':worldsize/5,
-'FFFF00':worldsize/5
-});
-//console.log('GOL');
-
-//public world is the world that is communicated to the clients
-var publicWorld = getAWorld(x,y);
-
-
-
-var wayOfLife = function (cWorld, callback, timeout)
-{
-  //console.log('WOF')
-  //var newWorld = [];
-  var newWorld = getAWorld(x,y);
-  for(var i = 0; i<cWorld.length; i++)
-  {
-    if(cWorld[i])
-    {
-       ////console.log(i);
+  return w
+};
+var world = getAWorld(x, y, {"a":worldsize / 5, "b":worldsize / 5, "c":worldsize / 5, "d":worldsize / 5, "e":worldsize / 5, "f":worldsize / 5, "g":worldsize / 5});
+var publicWorld = getAWorld(x, y);
+var wayOfLife = function(cWorld, callback, timeout) {
+  var newWorld = getAWorld(x, y);
+  for(var i = 0;i < cWorld.length;i++) {
+    if(cWorld[i]) {
     }
   }
-  
-  for(var i = 0; i<cWorld.length; i++)
-  {
-        for(var k = -1; k<=1; k++ )
-        {
-          for(var j = 1; j>=-1; j-- )
-          {
-            
-             var ni=0;
-
-              ni = i+((k*x)+j);
-              if(ni<0){ni=ni+worldsize}else if(ni>=worldsize) {ni=ni-worldsize}
-            
-             if(ni != i)
-             {
-              if(cWorld[ni].alive)
-              {
-                if(!newWorld[i]['ln'][cWorld[ni].color]){
-                newWorld[i]['ln'][cWorld[ni].color]=[ni];
-                }else{
-                newWorld[i]['ln'][cWorld[ni].color].push(ni);
-                }
-              }
-              //else dead neighbours
+  for(var i = 0;i < cWorld.length;i++) {
+    for(var k = -1;k <= 1;k++) {
+      for(var j = 1;j >= -1;j--) {
+        var ni = 0;
+        ni = i + (k * x + j);
+        if(ni < 0) {
+          ni = ni + worldsize
+        }else {
+          if(ni >= worldsize) {
+            ni = ni - worldsize
+          }
+        }
+        if(ni != i) {
+          if(cWorld[ni].alive) {
+            if(!newWorld[i]["ln"][cWorld[ni].party]) {
+              newWorld[i]["ln"][cWorld[ni].party] = [ni]
+            }else {
+              newWorld[i]["ln"][cWorld[ni].party].push(ni)
             }
           }
         }
-        
-        //start callculation next generation
-        var collisiontest = false;
-       //console.log(util.inspect(cWorld[i], true, null));
-        if(cWorld[i].alive)
-        {
-            var attackerwins = true;
-          //console.log('alive');
-            if(newWorld[i]['ln'])
-            {
-              for(var lncolor in newWorld[i]['ln'])
-              {
-                
-                //same color
-                if(lncolor == cWorld[i].color)
-                { 
-                  if(newWorld[i]['ln'][lncolor]&&newWorld[i]['ln'][lncolor].length==2||newWorld[i]['ln'][lncolor].length==3)
-                  {
-                    if(newWorld[i].alive&&attackerwins)
-                    {
-                    
-                    }
-                    else
-                    {
-                      newWorld[i].alive=truly;
-                      newWorld[i].color=lncolor;
-                    }
-                  }
-                 else
-                  {
-                   newWorld[i].alive=falsy;
-                   newWorld[i].color=undefined;
-                  }
-                }
-                else //other color
-                {
-                  //if((newWorld[i]['ln'][lncolor].length==3)&&(!newWorld[i]['ln'][cWorld[i].color]||newWorld[i]['ln'][cWorld[i].color]<3))
-                  if((newWorld[i]['ln'][lncolor].length==3))
-                  {
-                    if(newWorld[i].alive&&attackerwins==false)
-                    {
-                    
-                    }
-                    else
-                    {
-                    newWorld[i].alive=truly;
-                    newWorld[i].color=lncolor;
-                    }
-                   //newWorld[i].color='FF00FF';
-                  }
-                }
+      }
+    }
+    var collisiontest = false;
+    if(cWorld[i].alive) {
+      var attackerwins = true;
+      if(newWorld[i]["ln"]) {
+        for(var lnparty in newWorld[i]["ln"]) {
+          if(lnparty == cWorld[i].party) {
+            if(newWorld[i]["ln"][lnparty] && newWorld[i]["ln"][lnparty].length == 2 || newWorld[i]["ln"][lnparty].length == 3) {
+              if(newWorld[i].alive && attackerwins) {
+              }else {
+                newWorld[i].alive = true;
+                newWorld[i].party = lnparty
               }
+            }else {
+              newWorld[i].alive = false;
+              newWorld[i].party = undefined
             }
-            else
-            {
-              //has no living neighbors at all, dead
-              newWorld[i].alive=falsy;
-              newWorld[i].color=undefined;
-            }
-        
-           
-        }
-        else // not alive
-        {
-        
-          if(newWorld[i]['ln'])
-          {
-            for(var lncolor in newWorld[i]['ln'])
-            {
-              if(newWorld[i]['ln'][lncolor]&&newWorld[i]['ln'][lncolor].length==3)
-              {
-                 newWorld[i].alive=truly;
-                 newWorld[i].color=lncolor;
+          }else {
+            if(newWorld[i]["ln"][lnparty].length == 3) {
+              if(newWorld[i].alive && attackerwins == false) {
+              }else {
+                newWorld[i].alive = true;
+                newWorld[i].party = lnparty
               }
             }
           }
-         //  else it just stays dead
-       
         }
-      
-       ////console.log(i+' '+LifeNeighbours);
-    
+      }else {
+        newWorld[i].alive = false;
+        newWorld[i].party = undefined
+      }
+    }else {
+      if(newWorld[i]["ln"]) {
+        for(var lnparty in newWorld[i]["ln"]) {
+          if(newWorld[i]["ln"][lnparty] && newWorld[i]["ln"][lnparty].length == 3) {
+            newWorld[i].alive = true;
+            newWorld[i].party = lnparty
+          }
+        }
+      }
+    }
   }
   generation++;
-  //console.log('g:'+generation);
-  ////console.log(util.inspect(newWorld, true, null));
-    //now we emit the world
-    for(var i = 0; i<newWorld.length; i++)
-    {
-      publicWorld[i]=newWorld[i];
-    }
-    
-    //if(jQuery.isFunction(callback))
-    if(callback)
-    {
-      setTimeout(callback, timeout, newWorld, callback, timeout)
-    }
-    else
-    {
-      return newWorld;
-    }
-}
-
-var start = function(_w,_c,_t)
-{ var w,c,t;
-  if(_w){ w=_w; }else{ w=world; }
-  if(_c){ c=_c; }else{ c=wayOfLife; }
-  if(_t){ t=_t; wait=t; }else{ t=wait; }
-  if(active!==true)
-  {
-    world = wayOfLife(w, c, t);
+  for(var i = 0;i < newWorld.length;i++) {
+    publicWorld[i] = newWorld[i]
   }
-}
-
+  if(callback) {
+    setTimeout(callback, timeout, newWorld, callback, timeout)
+  }else {
+    return newWorld
+  }
+};
+var start = function(_w, _c, _t) {
+  var w, c, t;
+  if(_w) {
+    w = _w
+  }else {
+    w = world
+  }
+  if(_c) {
+    c = _c
+  }else {
+    c = wayOfLife
+  }
+  if(_t) {
+    t = _t;
+    wait = t
+  }else {
+    t = wait
+  }
+  if(active !== true) {
+    world = wayOfLife(w, c, t)
+  }
+};
 exports.start = start;
-exports.world = { 'x':x, 'y':y, 'g':generation, 't':wait, 'w':publicWorld };
-
-exports.getStreamlinedWorld = function()
-{
-  var streamlinedWorld=[];
-  
-  for(var i = 0; i < publicWorld.length; i ++)
-  {
-    if(!publicWorld[i].alive)
-    {
-      streamlinedWorld[i]=0;
-    }
-    else
-    {
-      if(colorO[publicWorld[i].color])
-      {
-        streamlinedWorld[i]=colorO[publicWorld[i].color];
-      }
-      else
-      {
+exports.world = {x:x, y:y, g:generation, t:wait, w:publicWorld};
+exports.getStreamlinedWorld = function() {
+  var streamlinedWorld = [];
+  for(var i = 0;i < publicWorld.length;i++) {
+    if(!publicWorld[i].alive) {
+      streamlinedWorld[i] = 0
+    }else {
+      if(partyO[publicWorld[i].party]) {
+        streamlinedWorld[i] = partyO[publicWorld[i].party]
+      }else {
         var j = 1;
-        for(var color in colorO)
-        {
-          j++;
+        for(var party in partyO) {
+          j++
         }
-        colorO[publicWorld[i].color]=j;
-        streamlinedWorld[i]=j;
+        partyO[publicWorld[i].party] = j;
+        streamlinedWorld[i] = j;
+        console.log(util.inspect(partyO))
       }
     }
   }
-  
-  return { 'x':x, 'y':y, 'g':generation, 't':wait, 'w':streamlinedWorld };
-}
-
-//world = wayOfLife(world, wayOfLife, 500);
+  return{x:x, y:y, g:generation, t:wait, w:streamlinedWorld}
+};
